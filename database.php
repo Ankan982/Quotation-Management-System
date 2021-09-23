dab1244d078c8e33d6e8674f4f434fa364c97ce6
Ankan Das <57554014+Ankan982@users.noreply.github.com>
2021-09-23 17:56:26 +0530

    Update database.php

--- database.php
+++ database.php
@@ -6,13 +6,6 @@ $username = "mykmpart_quote";
 $password ="S;H2l5w8I,@+";
 $db = "mykmpart_quote";
 
-//ip : 162.222.225.87
-//db_name: luxurvaf_addver
-//user_name: luxurvaf_addver
-//pass: J4z!3a5y
-
-//login: admin
-//pss: Abhishek@2021
 
 $conn = mysqli_connect($servername,$username,$password,$db);
 
